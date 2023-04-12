3186347db6cbaef47d82827cf37639c848cf12e2
Kuan314 <limzhengkuan@hotmail.com>
2023-04-12 17:26:06 +0800

    Combined Header to Info

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,11 @@
 import React from 'react';
 import './App.css';
 import AddTodo from './components/AddTodo';
-import Header from './components/Header';
 import Info from './components/Info';
 
 const App = () => {
   return (
     <div className="App">
-      <Header />
       <Info />
       <AddTodo />
     </div>
--- src/components/Info.jsx
+++ src/components/Info.jsx
@@ -1,14 +1,26 @@
 import React from "react";
 import "../App.css";
 import ProgressBar from "./ProgressBar";
-import Todo from "./Todo";
+import TodoList from "./TodoList";
 
 const Info = () => {
   return (
+    <>
     <div className="Info">
-      <ProgressBar />
-      <Todo />
+      <div className="Header">
+        <div className="Title">
+          Todo List
+        </div>
+        <div className="Description">
+          Add things to do
+        </div>
+      </div>
+      <div className="TodoList">
+        <ProgressBar />
+        <TodoList />
+      </div>
     </div>
+    </>
   )
 }
 
