439b4f1cf536816026368a6112609d2d2894bb29
kuan <kuan.ilinke@gmail.com>
2023-04-13 16:32:37 +0800

    add TodoList checkbox function

--- src/components/TodoList.jsx
+++ src/components/TodoList.jsx
@@ -1,7 +1,12 @@
 import React from "react";
 import "../App.css";
+import { FormGroup, FormControlLabel, Checkbox } from "@mui/material";
 
 const TodoList = () => {
+
+  const handleChange = (index) => {
+  };
+
   const data = [
     {
       task: "Eat",
@@ -14,17 +19,20 @@ const TodoList = () => {
   ];
 
   return (
-    <div className="Tasks">
-      {data.map((value) => {
+    <FormGroup className="Tasks">
+      {data.map((value, index) => {
         return (
-          <>
-          <div className="Task">
-            {value.task}
-          </div>
-          </>
+          <FormControlLabel className={`Task ${value.status ? "TaskDone" : ""}`} label={value.task} sx={{ margin: "5px 0"  }} 
+            control={
+              <Checkbox 
+                checked={value.status}
+                onChange={handleChange(index)}
+              />
+            }
+          />
         )
       })}
-    </div>
+    </FormGroup>
   )
 }
 
