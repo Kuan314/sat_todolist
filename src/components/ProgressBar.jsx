6970b2ccf87049ae88871c70fc0e0ec478184b7b
Kuan314 <limzhengkuan@hotmail.com>
2023-04-14 17:15:08 +0800

    implement progressBar

--- src/components/ProgressBar.jsx
+++ src/components/ProgressBar.jsx
@@ -1,10 +1,20 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import "../App.css";
 
-const ProgressBar = () => {
+const ProgressBar = ({ data }) => {
+  const totalTasks = data.length;
+  const [completeTasks, setCompleteTasks] = useState(0);
+
+  useEffect(() => {
+    const completedTasks = data.filter((task) => task.status).length;
+    setCompleteTasks(completedTasks);
+  }, [data]);
+
+  const completePercent = (completeTasks / totalTasks) * 100;
+
   return (
     <div>
-      progress bar
+      {completePercent}%
     </div>
   )
 }
